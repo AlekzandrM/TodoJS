337e61f5cccf7d96a292605a086f83042114501e
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-09-18 02:15:03 +0300

    created modal form, validation and functionality for adding new Todo

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,8 @@ class TodoList {
 
         for (let i = 0; i < this.arr.length; i++) {
             const todo = new TodoItem(this.arr[i]).showTodo()
+            let todoNum = i + 1 + '.'
+            todo.firstChild.prepend(todoNum)
             ul.append(todo)
         }
 
@@ -25,25 +27,196 @@ class TodoList {
     addTodo() {
         let myThis = this
         const inp = document.getElementById('newTodoInput')
+
         inp.addEventListener('keyup', function (e) {
             let text = this.value
 
             if (text.match(/^(?=.*[!@#$%^&(),.+=/\]\[{}?><":;|])/)) return
 
-            if (e.key == 'Enter' ) {
+            if (e.key === 'Enter' ) {
                 const start = new Date().toLocaleDateString()
                 const end = new Date(new Date().setDate(new Date().getDate()+1)).toLocaleDateString()
-                let newTodo = {id: todoList.length+1, message: text, start, end}
 
+                let newTodo = { message: text, start, end }
+                const todoLi = new TodoItem(newTodo).showTodo()
                 myThis.arr.push(newTodo)
 
-                const todoLi = new TodoItem(newTodo).showTodo()
+                let todoNum = myThis.arr.length + '.'
+                todoLi.firstChild.prepend(todoNum)
 
                 const ul = document.querySelector('ul')
                 ul.append(todoLi)
                 inp.value = ''
             }
+        })
+    }
 
+    closeModal() {
+        const modal = document.querySelector('.modal')
+        const btnCancel = document.querySelector('.modalCancel')
+        const btnSave = document.querySelector('.modalSave')
+
+        btnCancel.addEventListener('click', function () {
+            modal.classList.toggle('hide')
+        })
+        btnSave.addEventListener('click', function() {
+            modal.classList.toggle('hide')
+        })
+    }
+    openModal() {
+        const openButton = document.querySelector('.plus').firstElementChild
+        const modal = document.querySelector('.modal')
+        openButton.addEventListener('click', e => {
+            modal.classList.remove('hide')
+        })
+    }
+    addTodoFromModal() {
+        const myThis = this
+        const inpMessage = document.getElementById('modalTodoInput')
+        const inpStart = document.getElementById('start')
+        const inpEnd = document.getElementById('end')
+        const saveButton = document.querySelector('.modalSave')
+        let start = inpStart.value.split('-').reverse().join('.')
+        let end = inpStart.value.split('-').reverse().join('.')
+        let message = ''
+        const newTodo = {start, end, message}
+        const restrictedSymbols = ['!', '@', '#','$','%','^','&','*','(',')',]
+        let validMessage = false
+        let validDate = true
+        saveButton.setAttribute('disabled', 'disabled')
+
+        function showErr() {
+            let err = document.createElement('div')
+            err.classList.add('errDiv')
+            return err
+        }
+
+        function compareDates() {
+            if (new Date(inpStart.value).getTime() > new Date(inpEnd.value).getTime()) {
+                const errDiv = showErr()
+                errDiv.innerHTML = 'Wrong time intervals'
+                errDiv.classList.add('timeErr')
+                return errDiv
+            }
+        }
+
+        function showSaveButton(validMessage, validDate) {
+            if (validMessage && validDate) {
+                saveButton.classList.remove('modalSave')
+                saveButton.removeAttribute('disabled')
+            } else {
+                saveButton.classList.add('modalSave')
+                saveButton.setAttribute('disabled', 'disabled')
+            }
+        }
+        showSaveButton(validMessage, validDate)
+
+        inpMessage.addEventListener('input', function (e) {
+            for (let i = 0; i < restrictedSymbols.length; i++) {
+                let invalidInput = (this.value.split('').includes(restrictedSymbols[i]) || this.value === '')
+
+                if (invalidInput) {
+                    this.classList.add('invalid')
+                    const errDiv = showErr()
+                    errDiv.innerHTML = 'Wrong symbol or empty field'
+
+                    if (this.nextElementSibling === null) this.after(errDiv)
+                    validMessage = false
+                    showSaveButton(validMessage, validDate)
+                    return this
+                }
+                if (!invalidInput) {
+                    let errDiv = this.nextElementSibling
+                    if (errDiv) errDiv.remove()
+                    this.classList.remove('invalid')
+                    validMessage = true
+                    showSaveButton(validMessage, validDate)
+                }
+            }
+            newTodo.message = this.value
+            return validMessage
+        })
+
+        inpStart.addEventListener('change', function (e) {
+            start = this.value.split('-').reverse().join('.')
+            const errParentPosition = this.parentElement
+
+            const errDiv = compareDates()
+            const prevErr = errParentPosition.lastElementChild.classList.contains('errDiv')
+
+            if (errDiv || prevErr) {
+                validDate = false
+                showSaveButton(validMessage, validDate)
+            }
+            if (errDiv && prevErr) return this
+            if (prevErr) {
+                errParentPosition.lastChild.remove()
+                return this
+            }
+            if (errDiv && !prevErr) {
+                errParentPosition.append(errDiv)
+                return this
+            }
+            if (!errDiv && !prevErr) {
+                let errDiv = this.parentElement.lastElementChild
+                if (errDiv.classList.contains('errDiv')) errDiv.remove()
+
+                validDate = true
+                showSaveButton(validMessage, validDate)
+                newTodo.start = start
+            }
+
+            return validDate
+        })
+
+        inpEnd.addEventListener('change', function (e) {
+            end = this.value.split('-').reverse().join('.')
+            const errParentPosition = this.parentElement.previousElementSibling
+
+            let errDiv = compareDates()
+            const prevErr = this.parentElement.previousElementSibling.lastElementChild.classList.contains('errDiv')
+
+            if (errDiv || prevErr) {
+                validDate = false
+                showSaveButton(validMessage, validDate)
+            }
+            if (!errDiv && !prevErr) {
+                let errDiv = this.parentElement.previousElementSibling.lastElementChild
+                if (errDiv.classList.contains('errDiv')) errDiv.remove()
+                newTodo.end = end
+                validDate = true
+                showSaveButton(validMessage, validDate)
+                return this
+            }
+            if (errDiv && prevErr)  return this
+            if (prevErr) {
+                errParentPosition.lastChild.remove()
+
+                validDate = (new Date(inpStart.value).getTime() < new Date(inpEnd.value).getTime())
+                newTodo.start = inpStart.value.split('-').reverse().join('.')
+                newTodo.end = end
+                showSaveButton(validMessage, validDate)
+                return this
+            }
+            if (errDiv && !prevErr) {
+                errParentPosition.append(errDiv)
+                return this
+            }
+        })
+
+        saveButton.addEventListener('click', function (e) {
+            const modal = document.querySelector('.modal')
+            const todoLi = new TodoItem(newTodo).showTodo()
+
+            myThis.arr.push(newTodo)
+            let todoNum = myThis.arr.length + '.'
+            todoLi.firstChild.prepend(todoNum)
+
+            const ul = document.querySelector('ul')
+            ul.append(todoLi)
+            inpMessage.value = ''
+
+            modal.classList.add('hide')
         })
     }
 }
@@ -51,6 +224,11 @@ class TodoList {
 const myTodo = new TodoList(todoList)
 myTodo.createTodo()
 myTodo.addTodo()
+
+myTodo.closeModal()
+myTodo.openModal()
+myTodo.addTodoFromModal()
+
 
 
 
--- scripts/todoItem.js
+++ scripts/todoItem.js
@@ -1,16 +1,21 @@
 export class TodoItem {
-    constructor({id, message, done, start, end}) {
-        this.id = id
+    done = false
+    id = this.createId()
+
+    constructor({ message, start, end }) {
         this.message = message
-        this.done = done
         this.start = start
         this.end = end
+
     }
     showTodo() {
         const li = document.createElement('li')
-        li.innerHTML = `<span class="ind">${this.id}. ${this.message}</span> <span class="time">начало: ${this.start} конец: ${this.end}</span>`
+        li.innerHTML = `<span class="ind"> ${this.message}</span> <span class="time">начало: ${this.start} конец: ${this.end}</span>`
         let span = li.querySelector('.ind')
         span.style.fontWeight = 'bold'
         return li
+    }
+    createId() {
+        return (new Date().getTime()).toString()
     }
 }
